1f8174bc9acd7e3bf8a79d345bbbed2a2d8d3358
Mariengs <marianne.waago@hotmail.com>
2025-12-03 14:28:55 +0100

    safari fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+
+  // 🔧 Tving Vite til å alltid re-bundle deps i dev
+  optimizeDeps: {
+    force: true,
+    include: ["react", "react/jsx-dev-runtime", "react-router"],
+  },
 });
